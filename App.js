c9fd35272672cca2e5b783222c7d52bab2d93b7e
senparnasha <senparnasha@gmail.com>
2023-12-26 18:50:34 +0530

    shifted from vanila react code to jsx code

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
-// this is React element
-const heading = React.createElement("h1", {}, "Hello world from React");
-console.log(heading);
+import React from "react"
+import  ReactDOM  from "react-dom";
+
+const heading=(
+    <h2>This is react element</h2>
+)
+
+
+
+const HeadingComponent =()=>(
+    <div id="conrainer">
+{heading}
+<h1>Namaste React</h1>
+</div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
 
 

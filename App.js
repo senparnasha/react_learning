836963ae0f998b4803673106fc2b450bb2341b99
senparnasha <senparnasha@gmail.com>
2023-12-26 17:22:44 +0530

    second push

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
+// this is React element
 const heading = React.createElement("h1", {}, "Hello world from React");
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
+

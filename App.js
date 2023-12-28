ad60fac07ab08d8f36e969e58d88be6ca97bad09
senparnasha <senparnasha@gmail.com>
2023-12-28 15:40:18 +0530

    made card component

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import React from "react"
-import  ReactDOM  from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import logo from "./logo.jpg";
+import biriyani from "./biriyani.png"
 
-const heading=(
-    <h2>This is react element</h2>
-)
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={logo} alt="Logo" />;
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestroCard = () => {
+  return (
+    <div className="res-card">
+        <img className="card-image" alt="res-card-image" src={biriyani}/>
+      <h3>Meghna Food</h3>
+      <h4>Biriyani, North Indian, Asian</h4>
+      <h4>3.4 stars</h4>
+      <h4>25 minutes</h4>
 
 
+    </div>
+  );
+};
 
-const HeadingComponent =()=>(
-    <div id="conrainer">
-{heading}
-<h1>Namaste React</h1>
-</div>
-)
+const MainBody = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="restro-container">
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+        <RestroCard />
+
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <HeaderComponent />
+     <MainBody/>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
-
-
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # react_learning
+
+our app-
+Header
+-Logo
+-nav links
+Body
+-resturents container-resturent card
+Footer
+-copyright
+-links
+-address
+-contact

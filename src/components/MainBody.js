80d49bfd2769df6c53e78e19b32f8fba92983a73
senparnasha <senparnasha@gmail.com>
2024-01-04 11:48:18 +0530

    for filtering restro

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,17 +1,31 @@
 import React from "react";
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockData";
+import {useState} from "react"
 
 
 
 
 const MainBody = () => {
+const [listOfRestro,setListOfRestro]=useState(resList)
+
+  const handleTopRatedRestroFind=()=>{
+   const filteredRestro=listOfRestro.filter((resturants)=>{
+return resturants.info.avgRating > 4.5
+   })
+setListOfRestro(filteredRestro)
+  }
     return (
       <div className="body">
-        <div className="search">Search</div>
+        <div className="top-rated-restro">
+          <button className="top-rated-restro-btn"
+          onClick={handleTopRatedRestroFind}
+          
+          >Top Rated Resturants</button>
+        </div>
         <div className="restro-container">
-          {resList.map((eachRestro) => {
-            console.log("====>", eachRestro);
+          {listOfRestro.map((eachRestro) => {
+           
             return (
               <RestroCard key={eachRestro.info.id} restroData={eachRestro} />
             );

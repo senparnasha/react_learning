99fcce2e5f30807eeefd5f1dd984d163deb1b2a4
senparnasha <senparnasha@gmail.com>
2024-01-05 21:33:37 +0530

    search functionality added

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,8 +1,16 @@
 import React from "react";
 import logo from "../../logo.jpg";
-
+import {useState} from "react"
 
 const HeaderComponent = () => {
+const [btnName,setBtnName]=useState("Login")
+  const handleLoginBtn=()=>{
+if(btnName==="Login"){
+  setBtnName("Logout")
+}else{
+  setBtnName("Login")
+}
+  }
     return (
       <div className="header">
         <div className="logo-container">
@@ -14,6 +22,7 @@ const HeaderComponent = () => {
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <button onClick={handleLoginBtn} className="login-btn">{btnName}</button>
           </ul>
         </div>
       </div>
--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,57 +1,83 @@
 import React from "react";
 import RestroCard from "./RestroCard";
 
-import {useState,useEffect} from "react"
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
-
-
-
-
-
-
 const MainBody = () => {
-const [listOfRestro,setListOfRestro]=useState([])
+  const [listOfRestro, setListOfRestro] = useState([]);
+  const [filteredRestro, setFilteredRestro] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-useEffect(()=>{
-  console.log("useEffect")
-  fetchData()
-},[])
+  console.log("body rendered");
 
-const fetchData= async()=>{
-  const data= await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5466549&lng=88.3482327&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
-const json=await data.json()
-setListOfRestro(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-}
- const handleTopRatedRestroFind=()=>{
-   const filteredRestro=listOfRestro.filter((resturants)=>{
-return resturants.info.avgRating > 4.5
-   })
-setListOfRestro(filteredRestro)
-  }
+  useEffect(() => {
+    console.log("useEffect");
+    fetchData();
+  }, []);
 
-if(listOfRestro.length===0){
-  return(<><Shimmer/></>)
-}
-
-    return (
-      <div className="body">
-        <div className="top-rated-restro">
-          <button className="top-rated-restro-btn"
-          onClick={handleTopRatedRestroFind}
-          
-          >Top Rated Resturants</button>
-        </div>
-        <div className="restro-container">
-          {listOfRestro.map((eachRestro) => {
-           
-            return (
-              <RestroCard key={eachRestro.info.id} restroData={eachRestro} />
-            );
-          })}
-        </div>
-      </div>
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.5466549&lng=88.3482327&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestro(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestro(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
+  const handleTopRatedRestroFind = () => {
+    const filteredRestro = listOfRestro.filter((resturants) => {
+      return resturants.info.avgRating > 4.5;
+    });
+    setFilteredRestro(filteredRestro);
+  };
+  const handleSearch = () => {
+    const filteredRestro = listOfRestro.filter((restro) => {
+      return restro.info.name.toLowerCase().includes(searchText.toLowerCase());
+    });
+    setFilteredRestro(filteredRestro);
+  };
 
-  export default MainBody;
+  return listOfRestro.length === 0 ? (
+    <>
+      <Shimmer />
+    </>
+  ) : (
+    <div className="body">
+      <div className="top-rated-restro">
+        <div className="search">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search here"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button className="search-btn" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <button
+          className="top-rated-restro-btn"
+          onClick={handleTopRatedRestroFind}
+        >
+          Top Rated Resturants
+        </button>
+      </div>
+      <div className="restro-container">
+        {filteredRestro.map((eachRestro) => {
+          return (
+            <RestroCard key={eachRestro.info.id} restroData={eachRestro} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default MainBody;

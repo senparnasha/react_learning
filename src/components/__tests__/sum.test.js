42ae8f543f4a24093153af1bab3c3a37a7b7baa7
senparnasha <senparnasha@gmail.com>
2024-02-05 11:03:16 +0530

    for testing

---
+++ src/components/__tests__/sum.test.js
@@ -0,0 +1,18 @@
+import { sum } from "../sum"
+
+
+
+// test('should calculate the sum of two numbers ', () => {
+//     const result=sum(4,6);
+
+//     //Assertion
+//     expect(result).toBe(10)
+  
+// })
+
+test('should calculate the sum of two numbers', () => {
+    const result = sum(4, 6);
+  
+    // Assertion
+    expect(result).toBe(10);
+  });
